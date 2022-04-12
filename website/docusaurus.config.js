ea25252211d5ba35275e0908e9dedf784efd2ccf
Darius Mihai <dariusmihaim@gmail.com>
2022-04-12 14:39:32 +0300

    [Core] Fix document edit URL
    Signed-off-by: Darius Mihai <dariusmihaim@gmail.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          editUrl: 'https://gitlab.cs.pub.ro/scgc/courses/website/docs',
+          editUrl: 'https://gitlab.cs.pub.ro/scgc/cloud-courses/-/blob/main/website',
         },
         blog: false,
         theme: {
